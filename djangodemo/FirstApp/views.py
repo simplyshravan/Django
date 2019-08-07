612d7f996ea8661ee5a58a14fe417fea0c56ff48
Shravan Yadav <shravan.simply@gmail.com>
2019-08-07 18:20:35 +0530

    Pilot project in Django after 2 sessions
    Pilot project in Django after 2 sessions

---
+++ djangodemo/FirstApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+# Create your views here.
+def FirstApp_view(request,*args,**kwargs):
+    print(request.user)
+    #return HttpResponse("<h1>This is first App for django demo</h1>")
+    myinstance={'myvar':'10','myvar2':'hello20'
+                , 'myvar3':[10,20,'hi','bye']}
+    return render(request,'FirstApp.html',myinstance)
